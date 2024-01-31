6b14157a6981e0809332970bd8ad41ea8e1f7372
4lve <alve.jeansson@gmail.com>
2024-01-31 13:46:31 +0100

    DontDestroyOnLoad

--- Assets/Scripts/Global/GameState.cs
+++ Assets/Scripts/Global/GameState.cs
@@ -13,8 +13,9 @@ public class GameState : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        DontDestroyOnLoad(gameObject);
         infoText.text = "";
+
 
     }
 
