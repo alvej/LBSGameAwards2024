a2c1398d2c960badda1ac9945a91e6503a8df4b9
ohTame <44948346+ohTame@users.noreply.github.com>
2024-02-05 01:25:41 +0100

    Ny scen, lärar anims, dialogue press E grej

---
+++ Assets/Scripts/Dialogue/DialogueTeacher.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueTeacher : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float waitSeconds;
+    public GameObject dialogueSquare;
+
+    private int index;
+
+    public int dialogue = 0;
+    public bool inDialogue = false;
+
+    public GameState gameState;
+    private bool changedText = false;
+
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+    }
+
+
+
+
+    void Update()
+    {
+
+        if (Physics2D.OverlapCircle(transform.position, 1f, LayerMask.GetMask("Player")))
+        {
+            if (changedText == false)
+            {
+                changedText = true;
+                gameState.teacherTalkText.text = "Press E to talk to the Teacher";
+            }
+
+            if (Input.GetKeyDown(KeyCode.E) && dialogue == 0 && inDialogue == false)
+            {
+                StartDialogue();
+                inDialogue = true;
+            }
+        }
+        else
+        {
+            if (changedText == true)
+            {
+                changedText = false;
+                gameState.teacherTalkText.text = "";
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        dialogueSquare.SetActive(true);
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            inDialogue = false;
+            dialogueSquare.SetActive(false);
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(waitSeconds);
+        }
+    }
+}
--- Assets/Scripts/Global/GameState.cs
+++ Assets/Scripts/Global/GameState.cs
@@ -9,6 +9,7 @@ public class GameState : MonoBehaviour
 
 
     public TextMeshProUGUI infoText;
+    public TextMeshProUGUI teacherTalkText;
 
     // Start is called before the first frame update
     void Start()
